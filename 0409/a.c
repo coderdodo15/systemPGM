5322f1acd3bac6bdf8be3d1d80c63f6a56d7af2d
박성진 <pasuhn503@gmail.com>
2026-04-09 11:59:56 +0900

    makefile 실행

---
+++ 0409/a.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void func_a() {
+    printf("This is a.c function\n");
+}
---
+++ 0409/main.c
@@ -0,0 +1,12 @@
+// main.c
+#include <stdio.h>
+
+// 함수 선언
+void func_a();
+void func_b();
+
+int main() {
+    func_a();
+    func_b();
+    return 0;
+}
